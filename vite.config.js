daa46f6b7bc7f4d7aedf65745c725d177e62a85a
Aayush Raina <axr1147@student.bham.ac.uk>
2025-09-07 17:30:00 +0100

    MIME error resolution

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
   },
 })
